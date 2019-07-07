6bf9187ec1a0a4be2621c2cbff49622317e65554
esofar <973095739@qq.com>
2019-07-07 10:46:05 +0800

    Initial commit

--- .umirc.js
+++ .umirc.js
@@ -3,8 +3,8 @@
 export default {
   treeShaking: true,
   history: 'browser',
-  base: '/',
-  publicPath: './',
+  base: '/openmarks',
+  publicPath: '/openmarks/',
   outputPath: './docs',
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
